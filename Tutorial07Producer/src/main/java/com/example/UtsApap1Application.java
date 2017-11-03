21429567bdbb191e18ff772043f0ff1be25ae122
astidwiyana15 <dwiyanasti@gmail.com>
2017-11-03 20:00:16 +0700

    1

---
+++ Tutorial07Producer/src/main/java/com/example/UtsApap1Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtsApap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtsApap1Application.class, args);
+	}
+}
---
+++ Tutorial07Producer/src/main/java/com/example/service/StudentService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.model.CourseModel;
+import com.example.model.StudentModel;
+
+public interface StudentService
+{
+    StudentModel selectStudent (String npm);
+
+
+    List<StudentModel> selectAllStudents ();
+
+
+    void addStudent (StudentModel student);
+
+
+    void deleteStudent (String npm);
+    
+    void updateStudent(StudentModel student);
+    
+    List<CourseModel> selectCourses(String idCourse);
+    
+    CourseModel viewCourse(String idCourse);
+}
